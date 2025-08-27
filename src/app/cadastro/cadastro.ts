e11a471f2c4c529a9b553fe174c894fc0c0bb277
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-08-26 22:37:22 -0400

    Aula 27. Iniciando o Formulário de Clientes

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+     MatCardModule,
+     FormsModule,
+     MatFormFieldModule,
+     MatInputModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
 })
