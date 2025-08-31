e3978cee5cff8ef3483aaf436c19932e19fac2d6
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-08-31 11:54:23 -0400

    Aula 31. Criando o repositorio de clientes para persistir os dados

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -6,9 +6,22 @@ import { Cliente } from './cadastro/cliente';
 })
 export class ClienteService {
 
+  static REPO_CLIENTES = "_CLIENTES";
+
   constructor(){ }
 
   salvar(cliente: Cliente){
     console.log(cliente)
+  }
+
+  obterStorage():Cliente[]{
+    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
+    if (repositorioClientes){
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
+      return clientes;
+    }
+    const clientes: Cliente[] = [];
+    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(clientes));
+    return clientes;
   }
 }
